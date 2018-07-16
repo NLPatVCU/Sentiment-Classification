5199e657bb7bc8aa8f46fad3d2baea5db61b119b
maffeyl <31329104+maffeyl@users.noreply.github.com>
2018-07-16 19:43:38 -0400

    Added kfold back into keras, created webscraper.py

--- KerasSentiment.py
+++ KerasSentiment.py
@@ -1,3 +1,4 @@
+import numpy
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 # from gensim.models import word2vec
@@ -9,6 +10,9 @@ import csv
 import string
 import math
 import random
+
+from keras.utils import plot_model
+from sklearn.model_selection import StratifiedKFold
 
 np.random.seed(123)
 
@@ -31,10 +35,19 @@ def format_sentence(sent, swords=None):
                 filtered_words.append(w2v_model[word].tolist())
 
     # Sum all of the word vectors
+    # comment = [0.0 for _ in range(w2vsize)]
+    # for word in filtered_words:
+    #     for i in range(w2vsize):
+    #         comment[i] += word[i]
+
+    # Average all of the word vectors
     comment = [0.0 for _ in range(w2vsize)]
     for word in filtered_words:
         for i in range(w2vsize):
             comment[i] += word[i]
+    if len(filtered_words) > 0:
+        for i in range(w2vsize):
+            comment[i] = comment[i]/len(filtered_words)
 
     return comment
 
@@ -59,9 +72,9 @@ def import_csv(filename):
             c_stopwords = c_raw.read().translate(str.maketrans("", "", string.punctuation)).splitlines()
 
             if float(tmp_rating) <= float(args.n):
-                neg_list.append((format_sentence(tmp_com, stopwords), 'neg'))
+                neg_words.append((format_sentence(tmp_com, c_stopwords), 0))
             if float(tmp_rating) >= float(args.p):
-                pos_list.append((format_sentence(tmp_com, stopwords), 'pos'))
+                pos_words.append((format_sentence(tmp_com, c_stopwords), 1))
 
     # neg_words.extend(neg_words)
     # neg_words.extend(neg_words[:100])
@@ -81,7 +94,7 @@ parser.add_argument('-p', metavar='posratings', type=str, help='a list of positi
 parser.add_argument('-n', metavar='negratings', type=str, help='a list of negative ratings as strings',
                     required=False, default=['1', '2'])
 parser.add_argument('-z', metavar='iterations', type=str,
-                    help='the number of times to repeat the classifier training', required=False, default=1)
+                    help='the number of times to repeat the classifier training', required=False, default=2)
 parser.add_argument('-d', metavar='domain', type=str, help='a file with text from a different domain.',
                     required=False, default=None)
 parser.add_argument('-m', metavar='model', type=str, help='location of word2vec model to be used',
@@ -99,56 +112,104 @@ w2vsize = 300
 print("word2vec model {}".format(args.m))
 w2v_model = KeyedVectors.load_word2vec_format(args.m, binary=True)
 neg_list, pos_list = import_csv(args.i)
+# print(neg_list)
+seed = 123
+numpy.random.seed(seed)
+# dataset = neg_list.extend(pos_list)
+# print(dataset)
+# X = pos_list[:0]
+# print(X)
+# Y = pos_list[:1]
+# print(Y)
+negcutoff = math.floor(len(neg_list) * 1)
+poscutoff = math.floor(len(pos_list) * 1)
+neg_idx_train = sorted(random.sample(range(len(neg_list)), negcutoff))
+neg_train = [neg_list[i] for i in neg_idx_train]
 
-# Build keras NN
-model = Sequential()
-model.add(Dense(w2vsize, input_shape=(w2vsize, ), activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(w2vsize, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(w2vsize*2, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(w2vsize, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+pos_idx_train = sorted(random.sample(range(len(pos_list)), poscutoff))
+pos_train = [pos_list[i] for i in pos_idx_train]
+
+train = neg_train + pos_train
+
+X = numpy.array([x[0] for x in train])
+Y = numpy.array([x[1] for x in train])
+
+
+kfold = StratifiedKFold(n_splits=int(args.z), shuffle=True, random_state=seed)
+cvscores = []
+for train, test in kfold.split(X,Y):
+    model = Sequential()
+    model.add(Dense(w2vsize, input_shape=(w2vsize, ), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(w2vsize, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(w2vsize*2, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(w2vsize, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(X[train], Y[train], epochs=50, batch_size=20, verbose=0)
+    scores = model.evaluate(X[test], Y[test], verbose=0)
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+    cvscores.append(scores[1] * 100)
+    # plot_model(model, to_file='model.png')
+
+print("%.2f%% (+/- %.2f%%)" % (numpy.mean(cvscores), numpy.std(cvscores)))
 
 # create training and test sets
 # set the cutoffs
-negcutoff = math.floor(len(neg_list) * 3 / 4)
-poscutoff = math.floor(len(pos_list) * 3 / 4)
-
-print("Training {} times...".format(args.z))
-for z in range(int(args.z)):
-    # train = neg_list[:negcutoff] + pos_list[:poscutoff]
-    # test = neg_list[negcutoff:] + pos_list[poscutoff:]
-    neg_idx_train = sorted(random.sample(range(len(neg_list)), negcutoff))
-    neg_train = [neg_list[i] for i in neg_idx_train]
-
-    neg_idx_test = set(range(len(neg_list))) - set(neg_idx_train)
-    neg_test = [neg_list[i] for i in neg_idx_test]
-
-    pos_idx_train = sorted(random.sample(range(len(pos_list)), poscutoff))
-    pos_train = [pos_list[i] for i in pos_idx_train]
-
-    pos_idx_test = set(range(len(pos_list))) - set(pos_idx_train)
-    pos_test = [pos_list[i] for i in pos_idx_test]
-
-    train = neg_train + pos_train
-    test = neg_test + pos_test
-    print('Training on %d instances, testing on %d instances' % (len(train), len(test)))
-
-    train_data = [x[0] for x in train]
-    train_labels = [x[1] for x in train]
-    test_data = [x[0] for x in test]
-    test_labels = [x[1] for x in test]
-    model.fit(train_data, train_labels, epochs=20, batch_size=10)
-    scores = model.evaluate(test_data, test_labels)
-    print("Test data accuracy: {}".format(scores[1]*100))
-    # print("{}: {}".format(model.metrics_names[1], scores[1]*100))
+# negcutoff = math.floor(len(neg_list) * 3 / 4)
+# poscutoff = math.floor(len(pos_list) * 3 / 4)
+#
+# avgAccuracy = 0
+# print("Training {} times...".format(args.z))
+# for z in range(int(args.z)):
+#     # train = neg_list[:negcutoff] + pos_list[:poscutoff]
+#     # test = neg_list[negcutoff:] + pos_list[poscutoff:]
+#     neg_idx_train = sorted(random.sample(range(len(neg_list)), negcutoff))
+#     neg_train = [neg_list[i] for i in neg_idx_train]
+#
+#     neg_idx_test = set(range(len(neg_list))) - set(neg_idx_train)
+#     neg_test = [neg_list[i] for i in neg_idx_test]
+#
+#     pos_idx_train = sorted(random.sample(range(len(pos_list)), poscutoff))
+#     pos_train = [pos_list[i] for i in pos_idx_train]
+#
+#     pos_idx_test = set(range(len(pos_list))) - set(pos_idx_train)
+#     pos_test = [pos_list[i] for i in pos_idx_test]
+#
+#     train = neg_train + pos_train
+#     test = neg_test + pos_test
+#     print('Training on %d instances, testing on %d instances' % (len(train), len(test)))
+#
+#     train_data = [x[0] for x in train]
+#     train_labels = [x[1] for x in train]
+#     test_data = [x[0] for x in test]
+#     test_labels = [x[1] for x in test]
+#     model.fit(train_data, train_labels, epochs=20, batch_size=10)
+#     scores = model.evaluate(test_data, test_labels)
+#     avgAccuracy += scores[1]
+#     print("Test data accuracy: {}".format(scores[1]*100))
+#     # print("{}: {}".format(model.metrics_names[1], scores[1]*100))
+#
+# print("Average Accuracy: " + str(avgAccuracy / int(args.z)))
 
 # Import the file needing classification.
 if args.c is not None:
+    model = Sequential()
+    model.add(Dense(w2vsize, input_shape=(w2vsize,), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(w2vsize, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(w2vsize * 2, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(w2vsize, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(X, Y, epochs=50, batch_size=20, verbose=0)
+
     with open(args.c) as file:
         toclass = file.readlines()
     print("Predicting...")
@@ -162,6 +223,7 @@ if args.c is not None:
     for i in range(len(predictions)):
         answers.append([predictions[i], sentences[i]])
         print("{} :: {}".format(predictions[i], sentences[i]))
+
 
 if args.d is not None:
     domain_list = []
@@ -185,6 +247,19 @@ if args.d is not None:
 
     d_data = [word[0] for word in d_list]
     d_labels = [word[1] for word in d_list]
+
+    model = Sequential()
+    model.add(Dense(w2vsize, input_shape=(w2vsize,), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(w2vsize, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(w2vsize * 2, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(w2vsize, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(X, Y, epochs=50, batch_size=20, verbose=0)
 
     domain_accuracy = model.evaluate(d_data, d_labels)
     print("Classifier domain shift accuracy: {}".format(domain_accuracy[1]*100))
