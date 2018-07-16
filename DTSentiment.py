f91e3823a0a37ded9da68dc71d7e9e8a8b76052e
maffeyl <31329104+maffeyl@users.noreply.github.com>
2018-07-16 19:54:54 -0400

    Copied kfold validation back into DT

--- DTSentiment.py
+++ DTSentiment.py
@@ -6,6 +6,8 @@
 ### 
 
 import string
+
+import numpy
 from nltk.classify import DecisionTreeClassifier
 import nltk.classify.util
 import nltk
@@ -14,7 +16,7 @@ import csv
 import argparse
 import random
 from collections import Counter
-
+from sklearn.model_selection import StratifiedKFold
 
 ## Formats a string for input in the NB Classifier by converting all to lowercase and removing al punctuation.
 # @author Amy Olex
@@ -81,55 +83,83 @@ if __name__ == "__main__":
             if float(tmp_rating) >= float(args.p):
                 pos_list.append((format_sentence(tmp_com, stopwords), 'pos'))
 
-    
-    print("Total Negative Instances:"+str(len(neg_list))+"\nTotal Positive Instances:"+str(len(pos_list)))
-    
+    seed = 123
+    numpy.random.seed(seed)
+    print("Total Negative Instances:" + str(len(neg_list)) + "\nTotal Positive Instances:" + str(len(pos_list)))
+    negcutoff = math.floor(len(neg_list) * 1)
+    poscutoff = math.floor(len(pos_list) * 1)
+    neg_idx_train = sorted(random.sample(range(len(neg_list)), negcutoff))
+    neg_train = [neg_list[i] for i in neg_idx_train]
+
+    pos_idx_train = sorted(random.sample(range(len(pos_list)), poscutoff))
+    pos_train = [pos_list[i] for i in pos_idx_train]
+
+    dataset = neg_train + pos_train
+
+    X = [x[0] for x in dataset]
+    Y = [x[1] for x in dataset]
+    kfold = StratifiedKFold(n_splits=int(args.z), shuffle=True, random_state=seed)
+    cvscores = []
+    for train, test in kfold.split(X, Y):
+        # print(dataset[train[0]])
+        train_data = []
+        for i in range(len(train)):
+            train_data.append(dataset[train[i]])
+        test_data = []
+        for i in range(len(test)):
+            test_data.append(dataset[test[i]])
+        model = DecisionTreeClassifier.train(train_data)
+        scores = nltk.classify.util.accuracy(model, test_data)
+        print("{}%".format(scores * 100))
+        cvscores.append(scores * 100)
+        # plot_model(model, to_file='model.png')
+
+    print("%.2f%% (+/- %.2f%%)" % (numpy.mean(cvscores), numpy.std(cvscores)))
     ### create training and test sets
     ## set the cutoffs
-    negcutoff = math.floor(len(neg_list)*3/4)
-    poscutoff = math.floor(len(pos_list)*3/4)
+    # negcutoff = math.floor(len(neg_list)*3/4)
+    # poscutoff = math.floor(len(pos_list)*3/4)
+    #
+    # top10list = []
+    # avgAccuracy = 0
+    # for z in range(int(args.z)):
+    #     #train = neg_list[:negcutoff] + pos_list[:poscutoff]
+    #     #test = neg_list[negcutoff:] + pos_list[poscutoff:]
+    #     neg_idx_train = sorted(random.sample(range(len(neg_list)), negcutoff))
+    #     neg_train = [neg_list[i] for i in neg_idx_train]
+    #
+    #     neg_idx_test = set(range(len(neg_list))) - set(neg_idx_train)
+    #     neg_test = [neg_list[i] for i in neg_idx_test]
+    #
+    #
+    #     pos_idx_train = sorted(random.sample(range(len(pos_list)), poscutoff))
+    #     pos_train = [pos_list[i] for i in pos_idx_train]
+    #
+    #     pos_idx_test = set(range(len(pos_list))) - set(pos_idx_train)
+    #     pos_test = [pos_list[i] for i in pos_idx_test]
+    #
+    #     train = neg_train + pos_train
+    #     test = neg_test + pos_test
+    #     print('Training on %d instances, testing on %d instances' % (len(train), len(test)))
+    #
+    #     classifier = DecisionTreeClassifier.train(train)
+    #     accuracy = nltk.classify.util.accuracy(classifier, test)
+    #     avgAccuracy = avgAccuracy + accuracy
+    #     print('Classifier accuracy:', accuracy)
+    #
+    if args.c is not None:
+        model = DecisionTreeClassifier.train(dataset)
+        with open(args.c) as file:
+            toclass = file.readlines()
 
-    top10list = []
-    avgAccuracy = 0
-    for z in range(int(args.z)):
-        #train = neg_list[:negcutoff] + pos_list[:poscutoff]
-        #test = neg_list[negcutoff:] + pos_list[poscutoff:]
-        neg_idx_train = sorted(random.sample(range(len(neg_list)), negcutoff))
-        neg_train = [neg_list[i] for i in neg_idx_train]
+        for sent in toclass:
+            print(str(model.classify(format_sentence(sent))) + " :: " + sent)
+    #
+    # ### Count the occurences of each word that appeared in the top 10 over the 20 runs.
+    # print("Average Accuracy: "+ str(avgAccuracy/int(args.z)))
+    # my_counts = Counter(top10list)
+    # print(my_counts)
 
-        neg_idx_test = set(range(len(neg_list))) - set(neg_idx_train)
-        neg_test = [neg_list[i] for i in neg_idx_test]
-
-
-        pos_idx_train = sorted(random.sample(range(len(pos_list)), poscutoff))
-        pos_train = [pos_list[i] for i in pos_idx_train]
-
-        pos_idx_test = set(range(len(pos_list))) - set(pos_idx_train)
-        pos_test = [pos_list[i] for i in pos_idx_test]
-
-        train = neg_train + pos_train
-        test = neg_test + pos_test
-        print('Training on %d instances, testing on %d instances' % (len(train), len(test)))
-
-        classifier = DecisionTreeClassifier.train(train)
-        accuracy = nltk.classify.util.accuracy(classifier, test)
-        avgAccuracy = avgAccuracy + accuracy
-        print('Classifier accuracy:', accuracy)
-    
-        ### Import the file needing classification.
-        if args.c is not None:
-            with open(args.c) as file:
-                toclass = file.readlines()
-    
-            for sent in toclass:
-                print(classifier.classify(format_sentence(sent))+" :: "+sent)
-    
-    ### Count the occurences of each word that appeared in the top 10 over the 20 runs.
-    print("Average Accuracy: "+ str(avgAccuracy/int(args.z)))
-    my_counts = Counter(top10list)
-    print(my_counts)
-    
-    
     if args.d is not None:
         domain_list = []
         with open(args.d) as domainfile:
@@ -137,24 +167,19 @@ if __name__ == "__main__":
             for row in reader:
                 domain_list.append({'comment': row['comment'], 'rating': row['rating']})
         print(str(len(domain_list)))
-        
+
         d_list = []
         for c in range(len(domain_list)):
             tmp_c = domain_list[c]['comment']
             tmp_r = domain_list[c]['rating']
 
             if tmp_r in args.n:
-                d_list.append((format_sentence(tmp_c, stopwords), 'neg'))
+                d_list.append((format_sentence(tmp_c, stopwords), 0))
             if tmp_r in args.p:
-                d_list.append((format_sentence(tmp_c, stopwords), 'pos'))
+                d_list.append((format_sentence(tmp_c, stopwords), 1))
 
-        domain_accuracy = nltk.classify.util.accuracy(classifier, d_list)
+        # classifier2 = NaiveBayesClassifier.train(domain_list)
+        model = DecisionTreeClassifier.train(dataset)
+        domain_accuracy = nltk.classify.util.accuracy(model, d_list)
         print('Classifier domain shift accuracy:', domain_accuracy)
-        
-        
-           
-            
-            
-            
-            
             
